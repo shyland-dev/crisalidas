f559b75ca11c558802c3643266dbcc6d6c5f4040
dudushy <eduardotalarico1337@gmail.com>
2023-09-17 17:45:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+[![pages-build-deployment](https://github.com/shyland-dev/crisalidas/actions/workflows/pages/pages-build-deployment/badge.svg?branch=gh-pages)](https://github.com/shyland-dev/crisalidas/actions/workflows/pages/pages-build-deployment)
+
+# [crisalidas](https://shyland-dev.github.io/crisalidas/)
+Artesanato em PVC: Luminárias, Brindes, Troféus, Placas Decorativas e Chaveiros.
+
+---
+## Prerequisites:
+- [NodeJS](https://nodejs.org/)
+
+## How to install:
+1. Install packages
+    ```bash
+    npm i
+    ```
+2. Run project
+    ```bash
+    npm start
+    ```
+3. Build project
+    ```bash
+    npm run build
+    ```
+4. Deploy project (gh-pages)
+    ```bash
+    npm run deploy
+    ```
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TestComponent } from './pages/test/test.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TestComponent,
+    NotFoundComponent,
+    SobreComponent,
+    CatalogoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
